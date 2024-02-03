dc23c69be85fc53da7117ed76a434490439849bc
shohag0007 <merinedrive007@gmail.com>
2024-02-03 23:45:18 +0600

    Find out duplicate array

--- no-duplicate.js
+++ no-duplicate.js
@@ -10,7 +10,14 @@ const biryaniKhor = [ 'abul', 'babul', 'kabul', 'abul', 'babul', 'dabul', 'kabul
 const numbers = [1, 5, 61, 5, 87, 7, 5, 81, 61];
 
 function noDuplicate(array){
-    console.log(array);
+    const unique = [];
+   for(const item of array){
+    if(unique.includes(item) === false){
+        unique.push(item);
+    }
+
+   }
+   return unique;
 }
 
 const uniqueArray = noDuplicate(biryaniKhor);
