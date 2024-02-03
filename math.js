a9b92fa5e92219bcc92ac080dfdce31616c5c0bf
shohag0007 <merinedrive007@gmail.com>
2024-02-04 00:23:27 +0600

    Mathametical Items

---
+++ math.js
@@ -0,0 +1,13 @@
+
+const min = Math.min(12, 2, 13, 45, 78, 90);
+console.log(min);
+
+const max = Math.max(12, 2, 13, 45, 78, 90);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(88));
+console.log(Math.abs(-88));
+console.log(Math.round(2.49));
+console.log(Math.floor(2.90));
+console.log(Math.ceil(2.10))
