13cdf48d273b546e5f56298dccec57d78f6634f2
shohag0007 <merinedrive007@gmail.com>
2024-02-03 23:05:44 +0600

    complete the task

--- odd-average.js
+++ odd-average.js
@@ -1,10 +1,35 @@
 /*
 * function takes an array as parameter 
 * give me the average of odd numbers in the array.
-
-
 */
 
+/* put odd numbers in an array */
+
+
+
 function oddAverage(numbers){
-    
+  const odds = [];
+    for(const number of numbers){
+
+        if(number % 2 === 1){
+            odds.push(number);
+
+        }
+
+    }
+    // odds is the array that contains only the odd numbers.
+   // console.log(odds);
+
+   let sum = 0;
+   for(const number of odds){
+    sum = sum + number;
+   }
+   const count = odds.length;
+   console.log(sum, count);
+   const avg = sum / count;
+   return avg;
 }
+
+const numbers = [42, 13, 58, 65, 81, 96, 7];
+const avg = oddAverage(numbers);
+console.log('average of the odd numbers is :', avg);
