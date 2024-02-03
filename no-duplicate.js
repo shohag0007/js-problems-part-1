579704f671d17bbdc7557494c80b3b925fb7406f
shohag0007 <merinedrive007@gmail.com>
2024-02-03 23:30:23 +0600

    Duplicate array

---
+++ no-duplicate.js
@@ -0,0 +1,17 @@
+
+
+/* 
+* array has some duplicate elements.
+ * [] 
+ * 
+ */
+
+const biryaniKhor = [ 'abul', 'babul', 'kabul', 'abul', 'babul', 'dabul', 'kabul']
+const numbers = [1, 5, 61, 5, 87, 7, 5, 81, 61];
+
+function noDuplicate(array){
+    console.log(array);
+}
+
+const uniqueArray = noDuplicate(biryaniKhor);
+console.log(uniqueArray);
